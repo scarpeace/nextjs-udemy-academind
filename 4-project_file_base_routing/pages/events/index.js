a260d4f00762e287e786db31a4d161df6c5771a1
Gustavo Scarpellini <scarpellini1992@gmail.com>
2022-05-20 12:37:11 -0300

    submiting projeto of the file base routing module

---
+++ 4-project_file_base_routing/pages/events/index.js
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/event-search";
+import { getAllEvents } from "../../dummy-data";
+
+export default function EventsPage() {
+  const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
+  return (
+    <>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </>
+  );
+}
